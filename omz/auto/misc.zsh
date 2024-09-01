c2a0bf605eed62773b9b756b578b0438de7212f5
ojizero <hello@ojizero.tech>
2024-09-01 15:54:10 +0300

    corrected deleting whole words

--- omz/auto/misc.zsh
+++ omz/auto/misc.zsh
@@ -7,3 +7,10 @@ alias -- -='cd -'
 setopt multios              # enable redirect to multiple streams: echo >file1 >file2
 setopt long_list_jobs       # show long list format job notifications
 setopt interactivecomments  # recognize comments
+
+# WORDCHARS A list of non-alphanumeric characters considered part of a word
+# by the line editor.
+#
+# Set to nothing to make it make more sense + be more in line with how i'm used
+# to it coming from OhMyzsh.
+export WORDCHARS=''
